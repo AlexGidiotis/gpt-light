d5c6cd00d440bf36bf4d2f6278f4817592e0a8dc
agidiotis <alexgidiotis@gmail.com>
2023-04-19 09:44:30 +0000

    refactor test

--- test/integration/training/test_trainer.py
+++ test/integration/training/test_trainer.py
@@ -8,12 +8,9 @@ from src.data_io.data_loaders import DataLoader
 from src.training.train_main import init_job
 
 
-def test_scratch_training():
+def run_training_test(config_file):
     configs = init_job(
-        [
-            "--config_file",
-            os.path.join(os.path.dirname(__file__), "test_scratch_training.yml"),
-        ]
+        ["--config_file", os.path.join(os.path.dirname(__file__), config_file)]
     )
     os.rmdir(
         configs.job_config.out_dir
@@ -47,46 +44,11 @@ def test_scratch_training():
         out_dir = os.listdir(configs.job_config.out_dir)
         assert out_dir
         assert out_dir[0] == "ckpt.pt"
+
+
+def test_scratch_training():
+    run_training_test("test_scratch_training.yml")
 
 
 def test_finetune_training():
-    configs = init_job(
-        [
-            "--config_file",
-            os.path.join(os.path.dirname(__file__), "test_finetune_training.yml"),
-        ]
-    )
-    os.rmdir(
-        configs.job_config.out_dir
-    )  # this was created during init but we don't need it here
-    logging.debug(configs.job_config)
-
-    with TemporaryDirectory() as tmpdirname:
-        configs.job_config.out_dir = os.path.join(
-            tmpdirname, configs.job_config.out_dir
-        )
-        os.makedirs(
-            configs.job_config.out_dir, exist_ok=True
-        )  # we need to create this manually since it's a tempdir
-
-        data_loader = DataLoader(configs.data_config)
-        model_init = TrainModelInitialiser(configs)
-        initialised = model_init.init_model(data_loader)
-        configs = (
-            model_init.configs
-        )  # some configs might have been changed when loading a model
-
-        trainer = GPTTrainer(
-            configs.job_config, configs.model_config, initialised.checkpoint
-        )
-        trainer.init_trainer(initialised.model)
-        trainer.training_loop(
-            data_loader=data_loader,
-            model=initialised.model,
-            context=configs.context,
-            iter_num=initialised.iter_num,
-            best_val_loss=initialised.best_val_loss,
-        )
-        out_dir = os.listdir(configs.job_config.out_dir)
-        assert out_dir
-        assert out_dir[0] == "ckpt.pt"
+    run_training_test("test_finetune_training.yml")
